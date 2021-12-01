cfedc33337f5c0252a3841f860c58cf80e219c36
karelvanoordt <63868555+karelvanoordt@users.noreply.github.com>
2021-12-01 18:06:49 -0500

    Add Trump's image to app

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import {
   Routes,
 } from 'react-router-dom';
 import Navbar from './components/navbar';
+import Trump from './components/trump'
 import './App.css';
 
 function App() {
@@ -12,8 +13,8 @@ function App() {
     <Router>
       <Navbar />
       <Routes>
-        {/* <Route exact path="/" element={<Home />} />
-        <Route exact path="/calculator" element={<Calculator />} />
+        <Route exact path="/" element={<Trump />} />
+        {/* <Route exact path="/calculator" element={<Calculator />} />
         <Route exact path="/quote" element={<Quote />} /> */}
       </Routes>
     </Router>
---
+++ src/components/trump.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import trumpImg from './media/Trump-2.png';
+// import '/trump.css';
+// import TrumpOne from './media/Trump-1.png';
+// import TrumpTwo from './media/Trump-2.png';
+// import TrumpThree from './media/Trump-3.png';
+// import TrumpFour from './media/Trump-4.png';
+
+export default function Trump() {
+// CODE FOR RANDOM IMAGES GENERATOR STILL IN PROGRESS
+//     const images = [
+//         TrumpOne, TrumpTwo, TrumpThree, TrumpFour
+//     ]
+
+//     const RandomImage = Math.random(images);
+
+//     return (
+//         <img src={ require({ RandomImage }) } alt="Trump smiling" />
+//     )
+// }
+    return (
+        <div className="img-container">
+            <img className="trump-img" src={trumpImg} alt="BigCo Inc. logo"/>
+        </div>
+    )
+}
